55d3bd5d9f8da5e17d9be1723bdcd3365f308417
ymtoo <ymtoo86@gmail.com>
2020-06-26 18:09:34 +0800

    Add loss function

--- src/AdaptiveFilters.jl
+++ src/AdaptiveFilters.jl
@@ -3,9 +3,11 @@ using OnlineStats, DSP
 
 export adaptive_filter, focused_adaptive_filter, OMAP, MSPI, OMAS, ADAM, ExponentialWeight, EqualWeight
 
+import OnlineStats: l2regloss, l1regloss, logisticloss, l1hingeloss
+
 
 """
-    yh = adaptive_filter(y, alg=MSPI; order=4, lr=0.1)
+    yh = adaptive_filter(y, loss=l2regloss, alg=MSPI; order=4, lr=0.1)
 
 Filters `y` with an adaptive AR (only poles) filter with specified order.
 Returns `yh` which is the predicted output from an adaptive line enhancer (ALE). If your noise is wideband and signal narrowband, `yh` is your desired filtered signal. If the noise is narrowband and the signal is wideband, then `y-yh` is your desired filtered signal.
@@ -13,14 +15,15 @@ Returns `yh` which is the predicted output from an adaptive line enhancer (ALE).
 The first `order` samples of `yh` will be copies of `y`. The signals will thus have the same length.
 
 #Arguments:
+- `loss`: Loss function. Examples: `l2regloss, l1regloss, logisticloss, l1hingeloss`
 - `alg`: Stochastic approximation algorithm or weight function. Examples: `OMAP, MSPI, OMAS, ADAM, ExponentialWeight, EqualWeight`
 - `y`: Input signal
 - `order`: Filter order
 - `lr`: Learning rate or weight depending on `alg`
 """
-function adaptive_filter(y, alg::Type{<:OnlineStats.Algorithm}=MSPI; order=4, lr=0.1)
+function adaptive_filter(y, loss::Function=l2regloss, alg::Type{<:OnlineStats.Algorithm}=MSPI; order=4, lr=0.1)
     T = length(y)
-    model = StatLearn(order, alg(), rate=LearningRate(lr))
+    model = StatLearn(loss, order, alg(), rate=LearningRate(lr))
     yh = similar(y)
     yh[1:order] = y[1:order]
     for t in 1:T-order
@@ -30,6 +33,7 @@ function adaptive_filter(y, alg::Type{<:OnlineStats.Algorithm}=MSPI; order=4, lr
     end
     yh
 end
+adaptive_filter(y, alg::Type{<:OnlineStats.Algorithm}; kwargs...) = adaptive_filter(y, l2regloss, alg; kwargs...)
 
 function adaptive_filter(y, alg::Type{<:OnlineStats.Weight}; order=6, lr=0.2)
     T = length(y)
@@ -48,6 +52,7 @@ function adaptive_filter(y, alg::Type{<:OnlineStats.Weight}; order=6, lr=0.2)
     end
     yh
 end
+
 
 """
     focused_adaptive_filter(y, band, fs, args...; kwargs...)
